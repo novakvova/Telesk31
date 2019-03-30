1d04b45e21af8cd9fa7bc2e85d4b785a7840f4d0
novakvova <novakvova@gmail.com>
2019-03-30 10:23:50 +0200

    Add LoginWindow

--- TestApp/LoginWindow.xaml.cs
+++ TestApp/LoginWindow.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TestApp
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoginWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginWindow : Window
     {
-        public MainWindow()
+        public LoginWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            LoginWindow window = new LoginWindow();
-            window.ShowDialog();
         }
     }
 }
